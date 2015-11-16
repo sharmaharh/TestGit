aa279d25b7681bd1399ed84de81149099a21461e
Harsh Sharma <Harsh@Harshs-MacBook-Pro.local>
2015-11-16 09:02:28 +0530

    Created new project
    Here i created the fresh project

---
+++ TestGit/TestGit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestGit
+//
+//  Created by Harsh Sharma on 11/16/15.
+//  Copyright © 2015 IncredTechnologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestGit/TestGit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestGit
+//
+//  Created by Harsh Sharma on 11/16/15.
+//  Copyright © 2015 IncredTechnologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
